da7295e4d5869fca7d0757dfa9dbaa95916cdccd
Repith <119136592+Repith@users.noreply.github.com>
2023-08-18 20:24:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
